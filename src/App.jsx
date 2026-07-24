872e855eef6d46ac4a050c071b92a4159e75ebfd
raja18054 <157561804+raja18054@users.noreply.github.com>
2026-07-24 13:36:25 +0530

    Update and rename App.jsx to src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Testimonials from "./components/Testimonials";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="bg-slate-950 text-white min-h-screen">
+      <Navbar />
+      <Hero />
+      <Features />
+      <Testimonials />
+      <CTA />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
